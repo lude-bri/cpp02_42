dff6c0d0662ee05ce51ce3f93396239fdecc5d22
Luigi Piantavinha <szochoralewizc@hotmail.com>
2025-05-06 00:29:05 +0100

    cpp02

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -6,14 +6,37 @@
 /*   By: lude-bri <lude-bri@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 23:57:30 by lude-bri          #+#    #+#             */
-/*   Updated: 2025/05/05 23:58:28 by lude-bri         ###   ########.fr       */
+/*   Updated: 2025/05/06 00:25:11 by lude-bri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
-//constructor
-Fixed::Fixed(){}
+//default constructor
+Fixed::Fixed(){
+	std::cout << "Default constructor called" << std::endl;
+}
+
+//copy constructor
+Fixed::Fixed(const Fixed &obj) {
+	std::cout << "Copy constructor called" << std::endl;
+}
+
+//copy assignment operator overload
+Fixed &Fixed::operator=(const Fixed &obj) {
+	std::cout << "Copy assignment operator called" << std::endl;
+	return (*this);
+}
+
+//returns the raw value of the fixed-point value
+int	Fixed::getRawBits() const {
+	return (_fixedPoint);
+}
+
+//function that sets the raw value of the fixed-point number
+void	Fixed::setRawBits(int const raw) {
+	_fixedPoint = raw;
+}
 
 //destructor
 Fixed::~Fixed(){}
