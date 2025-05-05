dcb7d59251aa84aea1615c2fa5c0a5c28e0e8dd0
Luigi Piantavinha <szochoralewizc@hotmail.com>
2025-05-06 00:10:40 +0100

    cpp02 - begin

--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -6,11 +6,30 @@
 /*   By: lude-bri <lude-bri@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 22:32:38 by lude-bri          #+#    #+#             */
-/*   Updated: 2025/05/05 23:19:12 by lude-bri         ###   ########.fr       */
+/*   Updated: 2025/05/06 00:09:40 by lude-bri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_HPP
 # define FIXED_HPP
+
+#include <iostream>
+
+class Fixed {
+
+	private:
+		int					_fixedPoint;
+		static const int	_bits;
+
+	public:
+		Fixed();
+		Fixed(const Fixed &obj);
+		Fixed &operator=(const Fixed &obj);
+		~Fixed();
+
+		int			getRawBits() const;
+		void		setRawBits(int const raw);
+
+};
 
 #endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,20 +6,24 @@
 /*   By: lude-bri <lude-bri@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 19:51:05 by lude-bri          #+#    #+#             */
-/*   Updated: 2025/05/05 19:51:40 by lude-bri         ###   ########.fr       */
+/*   Updated: 2025/05/06 00:10:36 by lude-bri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
 #include <iostream>
 
-int main( void ) {
+int	main()
+{
 	Fixed a;
-	Fixed b( a );
+	Fixed b(a);
 	Fixed c;
+
 	c = b;
 
 	std::cout << a.getRawBits() << std::endl;
 	std::cout << b.getRawBits() << std::endl;
 	std::cout << c.getRawBits() << std::endl;
+	
 	return 0;
 }
