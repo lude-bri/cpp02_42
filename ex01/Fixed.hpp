8431fb89e78c93c9a84669df5c059dffe48be479
Luigi Piantavinha <szochoralewizc@hotmail.com>
2025-05-06 13:06:49 +0100

    cpp02 - ex01

--- ex01/Fixed.hpp
+++ ex01/Fixed.hpp
@@ -6,8 +6,29 @@
 /*   By: lude-bri <lude-bri@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 13:05:45 by lude-bri          #+#    #+#             */
-/*   Updated: 2025/05/06 13:05:46 by lude-bri         ###   ########.fr       */
+/*   Updated: 2025/05/06 13:06:44 by lude-bri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FIXED_HPP
+# define FIXED_HPP
 
+#include <iostream>
+
+class Fixed {
+
+	private:
+		int					_fixedPoint;
+		static const int	_bits = 8;
+
+	public:
+		Fixed();
+		Fixed(const Fixed &obj);
+		Fixed &operator=(const Fixed &obj);
+		~Fixed();
+
+		int			getRawBits() const;
+		void		setRawBits(int const raw);
+};
+
+#endif
