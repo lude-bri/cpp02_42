4c6f2efdb803bfa506a2dece2cd061f1d788cb64
Luigi Piantavinha <szochoralewizc@hotmail.com>
2025-05-07 01:05:37 +0100

    cpp02 - ex02

--- ex02/Fixed.cpp
+++ ex02/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: lude-bri <lude-bri@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 00:58:42 by lude-bri          #+#    #+#             */
-/*   Updated: 2025/05/07 00:58:50 by lude-bri         ###   ########.fr       */
+/*   Updated: 2025/05/07 01:05:18 by lude-bri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,28 @@
 
 //Default constructor
 Fixed::Fixed() : _fixedPoint(0) {
-	std::cout << "Default constructor called" << std::endl;
 }
 
 //copy constructor
 Fixed::Fixed(const Fixed &obj) {
-	std::cout << "Copy constructor called" << std::endl;
 	_fixedPoint = obj.getRawBits();
 }
 
 //Default destructor 
 Fixed::~Fixed() {
-	std::cout << "Destructor called" << std::endl;
 }
 
 //To Int constructor
 Fixed::Fixed(const int number) {
-	std::cout << "Int constructor called" << std::endl;
 	_fixedPoint = number << _bits; 
 }
 
 //To Float constructor
 Fixed::Fixed(const float number) {
-	std::cout << "Float constructor called" << std::endl;
 	_fixedPoint = roundf(number * (1 << _bits)); //arredondar para o inteiro mais prox
 }
 
 Fixed &Fixed::operator=(const Fixed &obj) {
-	std::cout << "Copy assignment operator called" << std::endl;
 	if (this != &obj) //vou verificar autoatribuicao a = a
 		_fixedPoint = obj.getRawBits();
 	return (*this);
