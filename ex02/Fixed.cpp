188afce5a300f5c58a47ba0e3b7a0e4db5d874c0
Luigi Piantavinha <szochoralewizc@hotmail.com>
2025-05-07 01:50:53 +0100

    cpp02 - ex02

--- ex02/Fixed.cpp
+++ ex02/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: lude-bri <lude-bri@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 00:58:42 by lude-bri          #+#    #+#             */
-/*   Updated: 2025/05/07 01:28:47 by lude-bri         ###   ########.fr       */
+/*   Updated: 2025/05/07 01:50:27 by lude-bri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,9 +40,82 @@ Fixed &Fixed::operator=(const Fixed &obj) {
 	return (*this);
 }
 
-//returns the raw value of the fixed-point value
-int	Fixed::getRawBits() const {
-	return (_fixedPoint);
+bool	Fixed::operator>(const Fixed &obj) const {
+	return _fixedPoint > obj._fixedPoint;
+}
+
+bool	Fixed::operator<(const Fixed &obj) const {
+	return _fixedPoint < obj._fixedPoint;
+}
+
+bool	Fixed::operator>=(const Fixed &obj) const {
+	return _fixedPoint >= obj._fixedPoint;
+}
+
+bool	Fixed::operator<=(const Fixed &obj) const {
+	return _fixedPoint <= obj._fixedPoint;
+}
+
+bool	Fixed::operator==(const Fixed &obj) const {
+	return _fixedPoint == obj._fixedPoint;
+}
+
+bool	Fixed::operator!=(const Fixed &obj) const {
+	return _fixedPoint != obj._fixedPoint;
+}
+
+Fixed	Fixed::operator+(const Fixed &obj) const {
+	return Fixed(this->toFloat() + obj.toFloat());
+}
+
+Fixed	Fixed::operator-(const Fixed &obj) const {
+	return Fixed(this->toFloat() - obj.toFloat());
+}
+
+Fixed	Fixed::operator*(const Fixed &obj) const {
+	return Fixed(this->toFloat() * obj.toFloat());
+}
+
+Fixed	Fixed::operator/(const Fixed &obj) const {
+	return Fixed(this->toFloat() / obj.toFloat());
+}
+
+Fixed	&Fixed::operator++() {
+	_fixedPoint++;
+	return *this;
+}
+
+Fixed	Fixed::operator++(int) {
+	Fixed temp(*this);
+	_fixedPoint++;
+	return temp;
+}
+
+Fixed	&Fixed::operator--() {
+	_fixedPoint--;
+	return *this;
+}
+
+Fixed	Fixed::operator--(int) {
+	Fixed temp(*this);
+	_fixedPoint--;
+	return temp;
+}
+
+Fixed	&Fixed::min(Fixed &a, Fixed &b) {
+	return (a < b) ? a : b;
+}
+
+const Fixed	&Fixed::min(const Fixed &a, const Fixed &b) {
+	return (a < b) ? a : b;
+}
+
+Fixed	&Fixed::max(Fixed &a, Fixed &b) {
+	return (a > b) ? a : b;
+}
+
+const Fixed	&Fixed::max(const Fixed &a, const Fixed &b) {
+	return (a > b) ? a : b;
 }
 
 //toFloat - converts fixed-point value to floating-point value
@@ -53,6 +126,11 @@ float	Fixed::toFloat() const {
 //toInt - converts fixed-point value to an integer value
 int		Fixed::toInt() const {
 	return _fixedPoint >> _bits;
+}
+
+//returns the raw value of the fixed-point value
+int	Fixed::getRawBits() const {
+	return (_fixedPoint);
 }
 
 //function that sets the raw value of the fixed-point number
