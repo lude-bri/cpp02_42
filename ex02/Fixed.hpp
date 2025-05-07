8db92a7a9cf888312b6a2a7d8989ecb60e8af7b5
Luigi Piantavinha <szochoralewizc@hotmail.com>
2025-05-07 01:25:04 +0100

    cpp02 - ex02

--- ex02/Fixed.hpp
+++ ex02/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: lude-bri <lude-bri@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 00:57:58 by lude-bri          #+#    #+#             */
-/*   Updated: 2025/05/07 00:58:09 by lude-bri         ###   ########.fr       */
+/*   Updated: 2025/05/07 01:24:53 by lude-bri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,9 +27,35 @@ class Fixed {
 		Fixed(const Fixed &obj);
 		Fixed(const int	number);
 		Fixed(const float number);
-		Fixed &operator=(const Fixed &obj);
 		~Fixed();
+	
+		//operadores de comparacao
+		Fixed &operator=(const Fixed &obj);
+		bool  operator>(const Fixed &obj) const;
+		bool  operator<(const Fixed &obj) const;
+		bool  operator>=(const Fixed &obj) const;
+		bool  operator<=(const Fixed &obj) const;
+		bool  operator==(const Fixed &obj) const;
+		bool  operator!=(const Fixed &obj) const;
 
+		//operadores aritmeticos
+		Fixed	operator+(const Fixed &obj) const;
+		Fixed	operator-(const Fixed &obj) const;
+		Fixed	operator*(const Fixed &obj) const;
+		Fixed	operator/(const Fixed &obj) const;
+
+		//operador de incremento/decremento
+		Fixed &operator++(); //pre-increment (++a)
+		Fixed operator++(int);
+		Fixed &operator--();
+		Fixed operator--(int);
+
+		//static memfunct
+		static Fixed &min(Fixed &a, Fixed &b);
+		static const Fixed &min(const Fixed &a, const Fixed &b);
+		static Fixed &max(Fixed &a, Fixed &b);
+		static const Fixed &max(const Fixed &a, const Fixed &b);
+		
 		int			getRawBits() const;
 		void		setRawBits(int const raw);
 		float		toFloat() const;
